d051734286ddca82dbb70f7f55bb66961aeca6b3
davebaol <davix7@yahoo.it>
2019-04-15 09:40:29 +0200

    Update dev dependencies and replace deprecated istanbul with nyc

--- test/index.js
+++ test/index.js
@@ -3,12 +3,12 @@ import defaultAwesomeFunction, { awesomeFunction } from '../src';
 
 describe('Awesome test.', () => {
   it('should test default awesome function', () => {
-    const expectedVal = 'I am the Default Awesome Function, fellow comrade! - Dinesh'
+    const expectedVal = 'I am the Default Awesome Function, fellow comrade! - Dinesh';
     assert(defaultAwesomeFunction('Dinesh') === expectedVal, 'Default not awesome :(');
   });
 
   it('should test awesome function', () => {
-    const expectedVal = 'I am just an Awesome Function'
+    const expectedVal = 'I am just an Awesome Function';
     assert(awesomeFunction() === expectedVal, 'Named awesome :(');
   });
 });
